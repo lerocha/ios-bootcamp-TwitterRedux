6d835e8276ee13e5ead209366179bb58729319f3
Luis Rocha <lerocha@gmail.com>
2017-04-22 18:17:17 -0700

    Implementing timeline

---
+++ TwitterRedux/TweetCell.swift
@@ -0,0 +1,76 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Luis Rocha on 4/16/17.
+//  Copyright © 2017 Luis Rocha. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    @IBOutlet weak var retweetedImageView: UIImageView!
+    @IBOutlet weak var retweetedLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var screennameLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var replyCountLabel: UILabel!
+    @IBOutlet weak var retweetCountLabel: UILabel!
+    @IBOutlet weak var favoritesCountLabel: UILabel!
+    
+    var tweet: Tweet! {
+        didSet {
+            retweetedLabel.isHidden = !tweet.retweeted
+            retweetedImageView.isHidden = !tweet.retweeted
+            if let imageUrl = tweet.user?.profileUrl {
+                profileImageView.setImageWith(imageUrl)
+            }
+            if let name = tweet.user?.name {
+                nameLabel.text = name
+            }
+            if let screenname = tweet.user?.screenname {
+                screennameLabel.text = "@" + screenname
+            }
+            if let timestamp = tweet.timestamp {
+                let hours = Calendar.current.dateComponents([.hour], from: timestamp, to: Date()).hour ?? 0
+                if hours > 0 {
+                    timestampLabel.text = String("\(hours)h")
+                } else {
+                    let minutes = Calendar.current.dateComponents([.minute], from: timestamp, to: Date()).minute ?? 0
+                    timestampLabel.text = String("\(minutes)m")
+                }
+            }
+            messageLabel.text = tweet.text
+            replyCountLabel.text = String("\(tweet.retweetCount)")
+            retweetCountLabel.text = String("\(tweet.retweetCount)")
+            favoritesCountLabel.text = String("\(tweet.favoritesCount)")
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        profileImageView.layer.cornerRadius = 3
+        profileImageView.clipsToBounds = true
+        
+        // sync up with actual size of the label
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // sync up with actual size of the label
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
